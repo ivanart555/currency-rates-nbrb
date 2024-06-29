2f463024b259e5a7aa6634a61d3e34557895018d
Ivan Artsiushyn <ivanart555@gmail.com>
2024-06-29 18:36:02 +0300

    Add exception handling for controller

--- src/main/java/com/vadarod/controller/RateController.java
+++ src/main/java/com/vadarod/controller/RateController.java
@@ -1,16 +1,17 @@
 package com.vadarod.controller;
 
 import com.vadarod.dto.RateDTO;
-import com.vadarod.entities.Rate;
+import com.vadarod.exception.RateServiceException;
 import com.vadarod.service.RateService;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/rates")
@@ -22,15 +23,25 @@ public class RateController {
     }
 
     @GetMapping("/update")
-    public String updateRates(@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-        rateService.updateRates(date);
-        return "Data loaded successfully for date: " + date;
+    public ResponseEntity<String> updateRates(@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        try {
+            rateService.updateRates(date);
+            return ResponseEntity.ok(String.format("Currency rates loaded successfully for date: %s", date));
+        } catch (RateServiceException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(String.format("Error updating rates: %s", e.getMessage()));
+        }
     }
 
     @GetMapping("/")
-    public RateDTO getRate(@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-                           @RequestParam("currencyCode") String currencyCode) {
-
-        return rateService.getCurrencyRate(date, currencyCode);
+    public ResponseEntity<?> getRate(@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+                                     @RequestParam("currencyCode") String currencyCode) {
+        try {
+            RateDTO rateDTO = rateService.getCurrencyRate(date, currencyCode);
+            return ResponseEntity.ok(rateDTO);
+        } catch (RateServiceException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(String.format("Error fetching currency rate for currency: %s and date: %s", currencyCode, date));
+        }
     }
 }
